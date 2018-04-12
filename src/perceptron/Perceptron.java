a4ca1000ef208bec126ecd99701f3f4eb4987e3f
DATAIMAGENES\mcotrina <miguel.cotrina@dataimagenes.pe>
2018-04-12 18:06:45 -0500

    Carga Metodo Display

--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -14,6 +14,58 @@ public class Perceptron {
     
     public static void main(String[] args) {
 
+        neuronaDisplay();
+        neuronaAndOr(1);
+        neuronaAndOr(2);
+    }
+    public static void neuronaAndOr(int tipo){
+        /*
+            NEURONA ENTRENADA AND Y OR
+        */
+        //valores esperados OR
+            List<Double> valoresEsperadoOR = new ArrayList<>();
+            valoresEsperadoOR.add(0.0);
+            valoresEsperadoOR.add(1.0);
+            valoresEsperadoOR.add(1.0);
+            valoresEsperadoOR.add(1.0);
+        //valores esperados AND
+            List<Double> valoresEsperadoAND = new ArrayList<>();
+            valoresEsperadoAND.add(0.0);
+            valoresEsperadoAND.add(1.0);
+            valoresEsperadoAND.add(1.0);
+            valoresEsperadoAND.add(1.0); 
+            
+         List<Double> valoresTomados = new ArrayList<>(); 
+         if(tipo==1){
+             valoresTomados=valoresEsperadoAND;
+         }else{
+             valoresTomados=valoresEsperadoOR;
+         }
+        //Todos los valores de entrada
+            List<Double> entrada1 = new ArrayList<>();entrada1.add(0.0); entrada1.add(0.0);
+            List<Double> entrada2 = new ArrayList<>();entrada2.add(0.0); entrada2.add(1.0);
+            List<Double> entrada3 = new ArrayList<>();entrada3.add(1.0); entrada3.add(0.0);
+            List<Double> entrada4 = new ArrayList<>();entrada4.add(1.0); entrada4.add(1.0);
+            List<List<Double>> entrada = new ArrayList<>();entrada.add(entrada1);entrada.add(entrada2);entrada.add(entrada3);entrada.add(entrada4);
+
+        //Creamos al entrenador
+            Trainer trainer = new Trainer();
+        //Creamos la neurona tamaño 2
+            Neuron neurona2 = new Neuron(2);
+        //Entrenamos la neurona
+            neurona2 = trainer.train(neurona2, entrada, valoresTomados);
+        //Cargamos los valores de prueba
+            List<Double> valoresX1 = new ArrayList<>();
+            valoresX1.add(1.0);
+            valoresX1.add(1.0);
+            neurona2.setX(valoresX1);
+        //Mostramos la salida
+        System.out.println(neurona2.toString());
+        System.out.println(neurona2.sinapsis());
+    }
+    
+    public static void neuronaEjemplo(){
+        
         /*
             NEURONA CREADA CON VALORES PREDIFINIDOS
         */
@@ -30,42 +82,121 @@ public class Perceptron {
             System.out.println(neurona1.toString());
             System.out.println(neurona1.sinapsis());
         
+    }
+    public static void neuronaDisplay(){
         /*
-            NEURONA ENTRENADA 
+            NEURONA ENTRENADA DE DISPLAY
+                 
+                 a
+                 _
+             b  |_|  c
+                 d
+             e  |_|  f
+                 g
         */
-        //valores esperados OR
-            List<Double> valoresEsperadoOR = new ArrayList<>();
-            valoresEsperadoOR.add(0.0);
-            valoresEsperadoOR.add(1.0);
-            valoresEsperadoOR.add(1.0);
-            valoresEsperadoOR.add(1.0);
-        //valores esperados AND
-            List<Double> valoresEsperadoAND = new ArrayList<>();
-            valoresEsperadoAND.add(0.0);
-            valoresEsperadoAND.add(1.0);
-            valoresEsperadoAND.add(1.0);
-            valoresEsperadoAND.add(1.0);    
+        List<Double> valoresEsperadoDP = new ArrayList<>();
+        valoresEsperadoDP.add(0.0);
+        valoresEsperadoDP.add(1.0);
+        valoresEsperadoDP.add(0.0);
+        valoresEsperadoDP.add(1.0);
+        valoresEsperadoDP.add(0.0);
+        valoresEsperadoDP.add(1.0);
+        valoresEsperadoDP.add(0.0);
+        valoresEsperadoDP.add(1.0);
+        valoresEsperadoDP.add(0.0);
+        valoresEsperadoDP.add(1.0);
+        
         //Todos los valores de entrada
-            List<Double> entrada1 = new ArrayList<>();entrada1.add(0.0); entrada1.add(0.0);
-            List<Double> entrada2 = new ArrayList<>();entrada2.add(0.0); entrada2.add(1.0);
-            List<Double> entrada3 = new ArrayList<>();entrada3.add(1.0); entrada3.add(0.0);
-            List<Double> entrada4 = new ArrayList<>();entrada4.add(1.0); entrada4.add(1.0);
-            List<List<Double>> entrada = new ArrayList<>();entrada.add(entrada1);entrada.add(entrada2);entrada.add(entrada3);entrada.add(entrada4);
-
+        List<Double> entrada0a = new ArrayList<>();
+                entrada0a.add(1.0); 
+        entrada0a.add(1.0);entrada0a.add(1.0);
+                entrada0a.add(0.0); 
+        entrada0a.add(1.0);entrada0a.add(1.0);
+                entrada0a.add(1.0);
+        List<Double> entrada1a = new ArrayList<>();
+                entrada1a.add(0.0); 
+        entrada1a.add(0.0);entrada1a.add(1.0);
+                entrada1a.add(0.0); 
+        entrada1a.add(0.0);entrada1a.add(1.0);
+                entrada1a.add(0.0);
+        List<Double> entrada2a = new ArrayList<>();
+                entrada2a.add(1.0); 
+        entrada2a.add(0.0);entrada2a.add(1.0);
+                entrada2a.add(1.0); 
+        entrada2a.add(1.0);entrada2a.add(0.0);
+                entrada2a.add(1.0);
+        List<Double> entrada3a = new ArrayList<>();
+                entrada3a.add(1.0); 
+        entrada3a.add(0.0);entrada3a.add(1.0);
+                entrada3a.add(1.0); 
+        entrada3a.add(0.0);entrada3a.add(1.0);
+                entrada3a.add(1.0);
+        List<Double> entrada4a = new ArrayList<>();
+                entrada4a.add(0.0); 
+        entrada4a.add(1.0);entrada4a.add(1.0);
+                entrada4a.add(1.0); 
+        entrada4a.add(0.0);entrada4a.add(1.0);
+                entrada4a.add(0.0);
+        List<Double> entrada5a = new ArrayList<>();
+                entrada5a.add(1.0); 
+        entrada5a.add(1.0);entrada5a.add(0.0);
+                entrada5a.add(1.0); 
+        entrada5a.add(0.0);entrada5a.add(1.0);
+                entrada5a.add(1.0); 
+        List<Double> entrada6a = new ArrayList<>();
+                entrada6a.add(1.0); 
+        entrada6a.add(1.0);entrada6a.add(0.0);
+                entrada6a.add(1.0); 
+        entrada6a.add(1.0);entrada6a.add(1.0);
+                entrada6a.add(1.0);                 
+        List<Double> entrada7a = new ArrayList<>();
+                entrada7a.add(1.0); 
+        entrada7a.add(0.0);entrada7a.add(1.0);
+                entrada7a.add(0.0); 
+        entrada7a.add(0.0);entrada7a.add(1.0);
+                entrada7a.add(0.0);  
+        List<Double> entrada8a = new ArrayList<>();
+                entrada8a.add(1.0); 
+        entrada8a.add(1.0);entrada8a.add(1.0);
+                entrada8a.add(1.0); 
+        entrada8a.add(1.0);entrada8a.add(1.0);
+                entrada8a.add(1.0);    
+        List<Double> entrada9a = new ArrayList<>();
+                entrada9a.add(1.0); 
+        entrada9a.add(1.0);entrada9a.add(1.0);
+                entrada9a.add(1.0); 
+        entrada9a.add(0.0);entrada9a.add(1.0);
+                entrada9a.add(1.0);                     
+        List<List<Double>> entradaa = new ArrayList<>();
+        entradaa.add(entrada0a);
+        entradaa.add(entrada1a);
+        entradaa.add(entrada2a);
+        entradaa.add(entrada3a);
+        entradaa.add(entrada4a);
+        entradaa.add(entrada5a);
+        entradaa.add(entrada6a);
+        entradaa.add(entrada7a);
+        entradaa.add(entrada8a);
+        entradaa.add(entrada9a);
+        
         //Creamos al entrenador
-            Trainer trainer = new Trainer();
+            Trainer trainer2 = new Trainer();
         //Creamos la neurona tamaño 2
-            Neuron neurona2 = new Neuron(2);
+            Neuron neurona3 = new Neuron(7);
         //Entrenamos la neurona
-            neurona2 = trainer.train(neurona2, entrada, valoresEsperadoOR);
+            neurona3 = trainer2.train(neurona3, entradaa, valoresEsperadoDP);
         //Cargamos los valores de prueba
-            List<Double> valoresX1 = new ArrayList<>();
-            valoresX1.add(1.0);
-            valoresX1.add(1.0);
-            neurona2.setX(valoresX1);
+            List<Double> valoresX2 = new ArrayList<>();
+                    valoresX2.add(1.0);
+            valoresX2.add(1.0); valoresX2.add(1.0);
+                    valoresX2.add(1.0);
+            valoresX2.add(0.0); valoresX2.add(1.0);
+                    valoresX2.add(1.0);
+
+            neurona3.setX(valoresX2);
         //Mostramos la salida
-        System.out.println(neurona2.toString());
-        System.out.println(neurona2.sinapsis());
+        System.out.println(neurona3.toString());
+        System.out.println(neurona3.sinapsis());
     }
     
 }
